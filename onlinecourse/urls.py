af8e9e7b4786950232a44e215c6916bead022dcc
PabloALencinas <pabloagus.lenci@gmail.com>
2023-09-01 21:08:37 -0400

    Adding submit view, extractanswer view, show exam view, calculating total score and updating the html file for exam result

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -17,8 +17,10 @@ urlpatterns = [
     # ex: /enroll/5/
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
-    # <HINT> Create a route for submit view
+    # Adding a route for submitting the exam
+    path('<int:course_id>/submit/', views.submit, name='submit'),
 
-    # <HINT> Create a route for show_exam_result view
+    # Show exam results with course ID and submission ID as parameters
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
 
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 # <HINT> Import any new Models here
-from .models import Course, Enrollment
+from .models import Course, Enrollment, Question, Choice, Submission
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
@@ -110,18 +110,37 @@ def enroll(request, course_id):
          # Collect the selected choices from exam form
          # Add each selected choice object to the submission object
          # Redirect to show_exam_result with the submission id
-#def submit(request, course_id):
+def submit(request, course_id):
+    # Getting user and course object
+    user = request.user
+    course = get_object_or_404(Course, pk=course_id)
+
+    # Getting the associated enrollment object created when the user enrrolled in the course
+    enrollment = get_object_or_404(Enrollment, user=user, course=course)
+
+    # Creating a submission object to the enrollment
+    submission = Submission.objects.create(enrollment=enrollment)
+
+    # Collecting the selected choices from the exam form
+    selected_choices = extract_answers(request)
+
+    # Adding each selected choice object to the submission object
+    for choice_id in selected_choices:
+        choice = get_object_or_404(Choice, id=choice_id)
+        submission.choices.add(choice)
+
+    # Redirecting to show_exam_result with the submission id
+    return redirect('show_exam_result', course_id=course_id, submission_id=submission.id)
 
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
-#def extract_answers(request):
-#    submitted_anwsers = []
-#    for key in request.POST:
-#        if key.startswith('choice'):
-#            value = request.POST[key]
-#            choice_id = int(value)
-#            submitted_anwsers.append(choice_id)
-#    return submitted_anwsers
+def extract_answers(request):
+    submitted_answers = []
+    for key, value in request.POST.items():
+        if key.startswith('choice'):
+            choice_id = int(value)
+            submitted_answers.append(choice_id)
+    return submitted_answers
 
 
 # <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
@@ -130,7 +149,70 @@ def enroll(request, course_id):
         # Get the selected choice ids from the submission record
         # For each selected choice, check if it is a correct answer or not
         # Calculate the total score
-#def show_exam_result(request, course_id, submission_id):
+def show_exam_result(request, course_id, submission_id):
+    # Get the course object and submission object based on their IDs
+    course = get_object_or_404(Course, pk=course_id)
+    submission = get_object_or_404(Submission, pk=submission_id)
+
+    # Get the selected choice IDs from the submission record
+    selected_choice_ids = submission.choices.values_list('id', flat=True)
+
+    # Initialize variables for total score and question results
+    total_score = 0
+    question_results = []
+
+    # Loop through all questions in the course
+    for question in course.question_set.all():
+        # Get the correct choice IDs for the question
+        correct_choice_ids = question.choice_set.filter(is_correct=True).values_list('id', flat=True)
+
+        # Check if the user's selected choice IDs match the correct choice IDs
+        is_correct = set(selected_choice_ids).issubset(set(correct_choice_ids))
+
+        # Calculate the question's grade
+        question_grade = question.grade if is_correct else 0
+
+        # Add the question result to the list
+        question_results.append({
+            'question_text': question.question_text,
+            'is_correct': is_correct,
+            'grade': question_grade,
+        })
+
+        # Add the question's grade to the total score
+        total_score += question_grade
+
+    # Determine if the learner passed the exam based on your passing criteria
+    # You can implement this logic based on the total score and passing threshold
+    passing_threshold = 80  # Score
+    passed_exam = total_score >= passing_threshold
+
+    # Add the course, selected choice IDs, total score, and question results to the context
+    context = {
+        'course': course,
+        'selected_ids': selected_choice_ids,
+        'total_score': total_score,
+        'question_results': question_results,
+        'passed_exam': passed_exam,
+    }
+
+    # Render the HTML page to display exam results
+    return render(request, 'onlinecourse/exam_result.html', context)
+
+
+# Calculating the total score for the exame
+
+def calculate_total_score(selected_choice_ids, course_id):
+    # Getting all the correct choices for the given course
+    correct_choices = Choice.objects.filter(question__course_id=course_id, is_correct=True)
+
+    # Converting the correct choice IDs to a set for efficient intersection
+    correct_choice_ids = set(correct_choices.values_list('id', flat=True))
+
+    # Calculating the total score by finding the intersection of correct and selected choice IDs
+    total_score = len(set(selected_choice_ids).intersection(correct_choice_ids))
+    
+    return total_score
 
 
 
